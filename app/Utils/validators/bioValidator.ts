9aadc516198f09fe1fb03248a760366974c539fd
Jerry S Joseph <jerin.sebastian153@gmail.com>
2023-05-13 19:59:34 +0100

    refractor update profile data

--- app/Utils/validators/bioValidator.ts
+++ app/Utils/validators/bioValidator.ts
@@ -1,7 +1,12 @@
-type StringValidator=(input:string)=>boolean|any
+type StringValidator=(input:string,...args:any[])=>boolean|any
 
 export const bioValidator:StringValidator=(input:string)=>{
     if(input.length>200)
         throw new Error('Your bio is too large. [max characters:200]')
         
+}
+
+export const NotEmptyStringValidator:StringValidator=(input:string,max:number=20)=>{
+    if(input.length>max || input.length===0)
+        throw new Error('Invalid input')
 }
--- app/ui/forms/EditProfileForm.tsx
+++ app/ui/forms/EditProfileForm.tsx
@@ -1,10 +1,11 @@
-import { Textarea, Button, TextInput, Avatar, FileInput, FileButton, Divider, Group } from '@mantine/core'
-import { saveBio } from 'data/api/profile'
+import { Textarea, Button, TextInput, Avatar, FileInput, FileButton, Divider, Group, Alert } from '@mantine/core'
+import { saveBio, updateProfile } from 'data/api/profile'
 import { useModal } from 'data/context/modal-context'
 import React, { useEffect, useState } from 'react'
 import { bioValidator } from '../../Utils/validators/bioValidator'
 import { UserProfile } from 'data/models/user'
 import { IconUpload, IconX } from '@tabler/icons'
+import { useAppContext } from 'data/context/app-context'
 
 interface IEditProfileForm {
     profile: UserProfile
@@ -14,33 +15,64 @@ const EditProfileForm = ({ profile }: IEditProfileForm) => {
     const [firstname, setFirstname] = useState<string>(profile.firstname)
     const [lastname, setLastname] = useState<string>(profile.lastname)
     const [username, setUsername] = useState<string>(profile.username)
+    const [email, setEmail] = useState<string>(profile.email)
     const [avatar, setAvatar] = useState<string>(profile.avatar)
-    const [file,setFile]=useState<File|null>(null)
+    const [file, setFile] = useState<File | null>(null)
 
     const [firstnameError, setFirstnameError] = useState<string>('')
     const [lastnameError, setLastnameError] = useState<string>('')
+    const [emailError, setEmailError] = useState<string>('')
 
-    const [error, setError] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(false)
-    const [imageLoading,setImageLoading]=useState<boolean>(false)
+    const [imageLoading, setImageLoading] = useState<boolean>(false)
+    
 
     const { closeModal } = useModal()
+    const {setError,setSuccess}=useAppContext()
+
+
+    function validateFields() {
+        if (firstname.length === 0 || firstname.length > 20) {
+            setFirstnameError('Invalid Firstname')
+            return false
+        }
+
+        if (lastname.length === 0 || lastname.length > 20) {
+            setLastnameError('Invalid Lastname')
+            return false
+        }
+
+        if (email.length === 0) {
+            setEmail('Invalid Email')
+            return false;
+        }
+
+        return true;
+    }
 
 
     async function handleSave() {
         try {
             setLoading(true)
-            bioValidator(firstname)
-            await saveBio(firstname)
+            if (!validateFields())
+                return
+
+            await updateProfile({
+                firstname,
+                lastname,
+                email
+            })
+            setSuccess('Profile updated successfully')
             closeModal()
+
         } catch (error) {
-            setError((error as Error).message)
+            setError(error as Error)
         } finally {
             setLoading(false)
         }
     }
 
-    function handleImageChange(f:File){
+    function handleImageChange(f: File) {
         console.log('image changed')
         setImageLoading(true)
         setAvatar(URL.createObjectURL(f))
@@ -50,15 +82,16 @@ const EditProfileForm = ({ profile }: IEditProfileForm) => {
 
     return (
         <div className="row">
-            <div className="col-12">               
+            
+            <div className="col-12">
                 <div className="d-flex w-100 justify-content-center">
-                    <Avatar src={avatar} size='xl' radius='xl'/>
+                    <Avatar src={avatar} size='xl' radius='xl' />
                 </div>
                 <div className="d-flex w-100 justify-content-center mt-2">
                     <FileButton onChange={handleImageChange} accept="image/*">
                         {(props) => <Button {...props} loading={imageLoading} compact leftIcon={<IconUpload size={15} />}>Upload Image</Button>}
                     </FileButton>
-                    <Button compact color='yellow' variant='outline' leftIcon={<IconX size={15} />} ml='lg' onClick={()=>{setAvatar(profile.avatar)}}>Remove image</Button>
+                    <Button compact color='yellow' variant='outline' leftIcon={<IconX size={15} />} ml='lg' onClick={() => { setAvatar(profile.avatar) }}>Remove image</Button>
                 </div>
                 <Divider my='md' />
             </div>
@@ -84,29 +117,29 @@ const EditProfileForm = ({ profile }: IEditProfileForm) => {
             <div className="col-lg-6 mt-2">
                 <TextInput
                     label='Email'
-                    value={firstname}
-                    onChange={e => setFirstname(e.target.value)}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                     required
-                    type='text'
-                    error={firstnameError} />
+                    type='email'
+                    error={emailError} />
             </div>
             <div className="col-lg-6 mt-2">
                 <TextInput
                     label='Username'
                     disabled
-                    value={lastname}
-                    onChange={e => setLastname(e.target.value)}
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
                     required
-                    type='text'
-                    error={lastnameError} />
+                    type='text' />
             </div>
+            
             <div className="col-12">
                 <Divider my='lg' />
             </div>
-            
+
             <div className="d-flex justify-content-between">
-                <Button color='gray' variant='outline'>Cancel</Button>
-                <Button >Save Profile</Button>
+                <Button color='gray' variant='outline' onClick={closeModal}>Cancel</Button>
+                <Button onClick={handleSave} loading={loading}>Save Profile</Button>
             </div>
         </div>
     )
